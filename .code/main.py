bb462555bfd548a664166d4918bc3d4cea2f32c9
Djeggis <Andrew.William.Gerungan@gmail.com>
2024-10-03 14:08:44 -0500

    Added the project proposal as well as the base python file
    title

---
+++ .code/main.py
@@ -0,0 +1,13 @@
+# Module 1: Deck Construction
+# 1. Scrape data from tournament reporting sites
+# 2. Categorize card categories
+# 3. Deck list input
+# 4. Calculate deck statistics
+# 5. NLP: Analyze differences between input deck and winning decks and give suggestions based off of list
+#    -Any immediate suggestions (specific card is missing that exists in majority of winning decks)
+#    -Recommend ratio changes (winning decks have certain ratios of card types)
+#    -Ask for anything specific the user wants to analyze
+# 
+# Modules 2: Rules Assistance
+# 1. Search for relevant results for specific comprehensive rules (input card name or specific card)
+# 2. NLP: Multi-step dialog for basic rules explanation (targeted towards beginners)
