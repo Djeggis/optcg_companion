187530a6d26d26d17b7bc51d171d7f16bae84bdd
Andrew G <112661737+Djeggis@users.noreply.github.com>
2024-11-27 15:22:39 -0600

    setup for decklist scraping
    basic setup for scraping from onepiecetopdecks.com

--- .code/main.py
+++ .code/main.py
@@ -11,3 +11,39 @@
 # Modules 2: Rules Assistance
 # 1. Search for relevant results for specific comprehensive rules (input card name or specific card)
 # 2. NLP: Multi-step dialog for basic rules explanation (targeted towards beginners)
+
+
+# Deck data scraping
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+
+driver.get("https://onepiecetopdecks.com/deck-list/japan-op-09-the-new-emperor-decks/")
+
+# finds all the links on page
+### NEED TO IMPLEMENT CHANGING PAGE ON TABLE ###
+table_trs = driver.find_elements(By.TAG_NAME, 'a')
+filtered_links = []
+
+# filter the deck list links out
+for x in table_trs:
+    if "deckgen" in x.get_attribute('href'):
+        filtered_links.append(x.get_attribute('href'))
+
+#print(filtered_links)
+
+
+for link in filtered_links:
+    link.click()
+
+    #find the tabletop textarea and grab
+    unfiltered_text = ...
+
+    #filter the text looping through
+
+# go through deck list links and find actual deck list and store
+# grab from tabletop simulator data (already nicely formatted), will further format later
+
+driver.quit()
+
