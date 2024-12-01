c8f148ca4e8fdb518b623142452fa477a3b7a0e9
Djeggis <Andrew.William.Gerungan@gmail.com>
2024-11-30 20:23:29 -0600

    Update deck2vec.py
    title

--- .code/deck2vec/deck2vec.py
+++ .code/deck2vec/deck2vec.py
@@ -94,8 +94,8 @@ def deck_to_data(text):
     # print(df)
     return df
 
-data = dataset_to_xarray("output.csv")
-deck = deck_to_data("1xOP01-060\n4xEB01-023\n4xOP02-054\n3xOP06-047\n4xOP07-040\n4xOP07-045\n4xOP07-046\n4xST03-004\n4xST03-005\n4xST17-002\n4xST17-003\n4xST17-004\n4xST17-005\n1xOP04-056\n2xOP07-057")
+# data = dataset_to_xarray("output.csv")
+# deck = deck_to_data("1xOP01-060\n4xEB01-023\n4xOP02-054\n3xOP06-047\n4xOP07-040\n4xOP07-045\n4xOP07-046\n4xST03-004\n4xST03-005\n4xST17-002\n4xST17-003\n4xST17-004\n4xST17-005\n1xOP04-056\n2xOP07-057")
 
 # make suggestion
 def suggestion(data, deck):
@@ -126,7 +126,7 @@ def suggestion(data, deck):
                     changes += f"{card}: +copies\n"
         else:
             changes += f"{card}: ?\n"
-    print(changes)
+    # print(changes)
     return changes
 
-suggestion(data, deck)
+# suggestion(data, deck)
